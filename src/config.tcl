7eec097faf857ee60c4ddf625ea55d14dfee69b8
Carlos Andrés Guerra <85963142+CarlosGS99@users.noreply.github.com>
2023-09-08 16:51:42 -0600

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -6,9 +6,9 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 set ::env(BASE_SDC_FILE) "./designs/multi_4bits/src/multi_4bits.sdc"
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
-set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PORT) "io_clk"
 
-set ::env(CLOCK_NET) "clk"
+set ::env(CLOCK_NET) "io_clk"
 set ::env(SYNTH_MAX_FANOUT) "12"
 set ::env(PL_RANDOM_INITIAL_PLACEMENT) "1"
 set ::env(FP_CORE_UTIL) "75"
