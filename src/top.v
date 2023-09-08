f51206946955d378ce4d6746349468596091ea6b
Carlos Andrés Guerra <85963142+CarlosGS99@users.noreply.github.com>
2023-09-08 11:48:39 -0600

    Add files via upload

---
+++ src/top.v
@@ -0,0 +1,10 @@
+`include "multi_4bits.v"
+
+module tt_um_carlosgs99_multi_4bits (
+    input wire [3:0]  A,
+    input wire [3:0]  B,
+    output wire [7:0] P
+);
+
+    multi_4bits multiplier(.A(A), .B(B), .Product_o(P));
+endmodule
